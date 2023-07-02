f17355e752302415a89ca74859eb8e3bf8ac7182
zakariamagdyz <zakariamagdy@ymail.com>
2023-07-02 12:16:22 +0300

    chore: add eslint rules

--- jest.config.mjs
+++ jest.config.mjs
@@ -16,6 +16,8 @@ const config = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+  resetMocks: false,
+  testTimeout: 30000,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -1,15 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import Navbar from "./Navbar";
+import { render, screen } from "@testing-library/react"
+
+import Navbar from "./Navbar"
 
 describe("Home", () => {
   it("renders a heading", () => {
-    render(<Navbar />);
+    render(<Navbar />)
 
     const authorName = screen.getByRole("link", {
       name: /Zakaria Magdy/i,
-    });
+    })
 
-    expect(authorName).toBeInTheDocument();
-  });
-});
+    expect(authorName).toBeInTheDocument()
+  })
+})
